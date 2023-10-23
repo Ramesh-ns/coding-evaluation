4bf9cf045a3c650427085580290cf966ff8d4c33
Ramesh-ns <ramesh.yenugula@gmail.com>
2023-10-23 12:11:31 -0700

    Implement hire() method in organization class

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -19,10 +19,46 @@ public abstract class Organization {
      * @param title
      * @return the newly filled position or empty if no position has that title
      */
-    public Optional<Position> hire(Name person, String title) {
-        //your code here
-        return Optional.empty();
-    }
+	public Optional<Position> hire(Name person, String title) {
+		return hirePosition(root, person, title);
+	}
+
+	/**
+	 * Recursively searches for the position with the given title and is not filled.
+	 * If it finds such a position, it creates a new employee and assigns them to
+	 * that position
+	 * 
+	 * @param currentPosition
+	 * @param person
+	 * @param title
+	 * 
+	 * @return An Optional containing the newly filled position, or empty if no
+	 *         position has that title.
+	 */
+	private Optional<Position> hirePosition(Position currentPosition, Name person, String title) {
+		if (currentPosition.getTitle().equals(title) && !currentPosition.isFilled()) {
+			Employee employee = new Employee(generateEmployeeId(), person);
+			currentPosition.setEmployee(Optional.of(employee));
+			return Optional.of(currentPosition);
+		} else {
+			for (Position position : currentPosition.getDirectReports()) {
+				Optional<Position> hiredPosition = hirePosition(position, person, title);
+				if (hiredPosition.isPresent()) {
+					return hiredPosition;
+				}
+			}
+		}
+		return Optional.empty();
+	}
+
+	/**
+	 * Generates a random employee ID
+	 * 
+	 * @return A randomly generated employee ID.
+	 */
+	private int generateEmployeeId() {
+		return (int) (Math.random() * 1000);
+	}
 
     @Override
     public String toString() {
